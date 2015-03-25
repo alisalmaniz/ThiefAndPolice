b900727b266d6d8406f93a3d68f000d8596bfbe6
alisalmaniz <ali_salmani15@yahoo.com>
2015-03-25 11:23:13 +0430

    project

--- src/thiefandpolice/ThiefAndPolice.java
+++ src/thiefandpolice/ThiefAndPolice.java
@@ -16,6 +16,8 @@ public class ThiefAndPolice {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Police police1 = new Police();
+        
     }
     
 }
