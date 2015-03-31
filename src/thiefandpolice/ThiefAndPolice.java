cd2846d747207a79cc8f1af1161fb88a148f0cb9
alisalmaniz <ali_salmani15@yahoo.com>
2015-03-31 19:00:34 +0430

    finalized

--- src/thiefandpolice/ThiefAndPolice.java
+++ src/thiefandpolice/ThiefAndPolice.java
@@ -17,7 +17,7 @@ public class ThiefAndPolice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here 
         
 
         Data data= new Data();
